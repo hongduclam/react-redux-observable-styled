f63f81dc893aa7c3d5de0be462eb28df74b53f81
hongduclamvt@gmail.com <bitbucket!@#$>
2019-05-19 12:39:38 +0700

    add prod

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 console.log('process.env', process.env);
-export const BACKEND_URL = process.env.NODE_ENV === 'development' ? `http://localhost:3000/app` : "https://api.giphy.com";
+export const BACKEND_URL = "https://api.giphy.com";
 
 export const STATE_NAME = {
   ITEM_LIST: "itemList"
